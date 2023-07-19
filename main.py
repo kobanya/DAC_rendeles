4461fcb82b308c1c5572d2cea5eb30a5bd80ab7e
bela <nagy.bela.budapest@gmail.com>
2023-07-19 22:49:10 +0200

    PANDAS használatával ugyanaz

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import csv
+import pandas as pd
 
 def process_orders(rendeles_file):
     rendeles_dict = {}
 
-    with open(rendeles_file, newline='', encoding='utf-8') as csvfile:
-        rendeles_reader = csv.reader(csvfile, delimiter=';')
-        for row in rendeles_reader:
-            if row[0] == 'M':
-                rendeles_szam = row[2]
-                rendeles_dict[rendeles_szam] = {'termek': [], 'mennyiseg': []}
-            elif row[0] == 'T':
-                rendeles_szam = row[1]
-                termek = row[2]
-                mennyiseg = int(row[3])
-                rendeles_dict[rendeles_szam]['termek'].append(termek)
-                rendeles_dict[rendeles_szam]['mennyiseg'].append(mennyiseg)
+    rendeles_df = pd.read_csv(rendeles_file, delimiter=';', encoding='utf-8')
+    rendeles_szam = None
+    for row in rendeles_df.itertuples(index=False):
+        if row[0] == 'M':
+            rendeles_szam = row[2]
+            rendeles_dict[rendeles_szam] = {'termek': [], 'mennyiseg': []}
+        elif row[0] == 'T' and rendeles_szam is not None and row[1] == rendeles_szam:
+            termek = row[2]
+            mennyiseg = int(row[3])
+            rendeles_dict[rendeles_szam]['termek'].append(termek)
+            rendeles_dict[rendeles_szam]['mennyiseg'].append(mennyiseg)
 
     return rendeles_dict
 
 rendeles_dict = process_orders('rendeles.csv')
 print(rendeles_dict)
-
